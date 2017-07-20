6f8306f98c30cb23f8647c59146518bd383eb0b6
Adam <ak2g15@soton.ac.uk>
2017-07-20 23:12:18 +0100

    Taking screenshots

--- MainView.py
+++ MainView.py
@@ -1,6 +1,6 @@
 import sys
 from PyQt5 import uic, QtWidgets
-from  PyQt5.QtWidgets import QPushButton
+import pyscreenshot as ImageGrab
 
 qtViewFile = "./Design/Main.ui"  # Enter file here.
 
@@ -9,6 +9,8 @@ Ui_MainWindow, QtBaseClass = uic.loadUiType(qtViewFile)
 
 class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self):
+        self.box = 0
+
         QtWidgets.QMainWindow.__init__(self)
         Ui_MainWindow.__init__(self)
         self.setupUi(self)
@@ -17,11 +19,29 @@ class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
 
         self.stop_button.clicked.connect(self.onclicked_stop)
 
+        self.fullsc_radb.setChecked(True)
+        self.fullsc_radb.toggled.connect(self.full_screen_radb)
+        self.boxsc_radb.toggled.connect(self.box_screen_radb)
+
+
     def onclicked_start(self):
-        print("start")
+        print("start", self.frequency_spin_box.value())
+        if isinstance(self.box, int):
+            im = ImageGrab.grab()
+        else:
+            im = ImageGrab.grab(self.box)
+        im.show()
 
     def onclicked_stop(self):
         print("stop")
+
+    def full_screen_radb(self):
+        if self.fullsc_radb.isChecked():
+            self.box = 0
+
+    def box_screen_radb(self):
+        if self.boxsc_radb.isChecked():
+            self.box = [20, 20, 100, 100]
 
 
 if __name__ == "__main__":
