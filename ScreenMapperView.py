e9128e6b625e70838ef732c01eaa9dc3213e468f
Kanter666 <ak2g15@soton.ac.uk>
2017-08-19 09:36:32 +0100

    Implemented ImageViewer

--- ScreenMapperView.py
+++ ScreenMapperView.py
@@ -1,5 +1,6 @@
 import sys
 
+from ImageViewerQt import ImageViewerQt
 from os import listdir
 from PyQt5 import uic, QtWidgets
 from PyQt5.QtCore import pyqtSignal
@@ -26,9 +27,8 @@ class ScreenMapperView(QtWidgets.QMainWindow, Ui_StartWindow):
         self.finish_bt.clicked.connect(self.print_arguments)
         self.screens_cb.currentIndexChanged.connect(self.screen_changed)
 
-        # Image is displayed as a QPixmap in a QGraphicsScene attached to this QGraphicsView.
-        self.scene = QtWidgets.QGraphicsScene()
-        self.image_view_gv.setScene(self.scene)
+        self.image_view = ImageViewerQt()
+        self.grid_layout.addWidget(self.image_view,0,0,10,1)
 
     def set_arguments(self, arguments):
         self.folder = arguments[0]
@@ -38,28 +38,9 @@ class ScreenMapperView(QtWidgets.QMainWindow, Ui_StartWindow):
         self.screens_cb.setCurrentIndex(0)
 
     def screen_changed(self, i):
-        image = QImage(self.folder+"/"+self.screens_cb.currentText())
-        if type(image) is QPixmap:
-            pixmap = image
-        elif type(image) is QImage:
-            pixmap = QPixmap.fromImage(image)
-        else:
-            raise RuntimeError("ImageViewer.setImage: Argument must be a QImage or QPixmap.")
-        if self.hasImage():
-            self._pixmapHandle.setPixmap(pixmap)
-        else:
-            self._pixmapHandle = self.scene.addPixmap(pixmap)
-        self.setSceneRect(QRectF(pixmap.rect()))  # Set scene size to image size.
-        self.updateViewer()
-
-    def updateViewer(self):
-        if not self.hasImage():
-            return
-        if len(self.zoomStack) and self.sceneRect().contains(self.zoomStack[-1]):
-            self.fitInView(self.zoomStack[-1], Qt.IgnoreAspectRatio)  # Show zoomed rect (ignore aspect ratio).
-        else:
-            self.zoomStack = []  # Clear the zoom stack (in case we got here because of an invalid zoom).
-            self.fitInView(self.sceneRect(), self.aspectRatioMode)  # Show entire image (use current aspect ratio mode).
+        image = self.folder+"/"+self.screens_cb.currentText()
+        self.image_view.loadImageFromFile(image)
+        print(self.folder+"/"+self.screens_cb.currentText())
 
     def print_arguments(self):
         print(self.text)
