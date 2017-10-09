904294baaa564ede78c074029e0f148b56120792
Kanter666 <ak2g15@soton.ac.uk>
2017-10-09 22:46:15 +0100

    Fliping and rotating at the same time fix

--- Models/Library.py
+++ Models/Library.py
@@ -124,15 +124,35 @@ class Library(object):
                             )
                         )
                         if "flip" in function.dictionary and function.dictionary["flip"]:
+
                             file.write("        if len(loc[0])>0:\n"
                                        "            return loc\n"
-                                       "        image = cv2.flip(image, 1)\n"
-                                       "        res = cv2.matchTemplate(cropped, image, cv2.TM_CCOEFF_NORMED)\n"
+                                       "        flipmage = cv2.flip(image, 1)\n"
+                                       "        res = cv2.matchTemplate(cropped, flipmage, cv2.TM_CCOEFF_NORMED)\n"
                                        "        threshold = 0.{}\n"
                                        "        loc = numpy.where( res >= threshold)\n".format(
                                 function.dictionary["match_threshold"]
                             )
                             )
+
+                            if "rotate" in function.dictionary and function.dictionary["rotate"]:
+                                file.write("        for angle in [90, 180, 270]:\n"
+                                           "            if len(loc[0])>0:\n"
+                                           "                return loc\n"
+                                           "            image = numpy.rot90(image)\n"
+                                           "            res = cv2.matchTemplate(cropped, image, cv2.TM_CCOEFF_NORMED)\n"
+                                           "            threshold = 0.{}\n"
+                                           "            loc = numpy.where( res >= threshold)\n"
+                                           "            if len(loc[0])>0:\n"
+                                           "                return loc\n"
+                                           "            flipmage = cv2.flip(image, 1)\n"
+                                           "            res = cv2.matchTemplate(cropped, flipmage, cv2.TM_CCOEFF_NORMED)\n"
+                                           "            threshold = 0.{}\n"
+                                           "            loc = numpy.where( res >= threshold)\n".format(
+                                    function.dictionary["match_threshold"], function.dictionary["match_threshold"]
+                                )
+                                )
+
                         elif "rotate" in function.dictionary and function.dictionary["rotate"]:
                             file.write("        for angle in [90, 180, 270]:\n"
                                        "            if len(loc[0])>0:\n"
